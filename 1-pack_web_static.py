c9960431c905b87680f425e5fb84108259cba001
olalekan <lekanmojibola@gmail.com>
2024-06-07 06:14:32 +0100

    made the code satisfy pylint

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,7 +12,8 @@ def do_pack():
     if not os.path.exists("versions"):
         os.mkdir("versions")
     time_of_crt = datetime.now()
-    archive_name = "web_static_{}.tgz".format(time_of_crt.strftime("%Y%m%d%H%M%S"))
+    archive_name = "web_static_{}.tgz".format(time_of_crt.
+                                              strftime("%Y%m%d%H%M%S"))
     local("tar -cvzf versions/{} web_static".format(archive_name))
 
     archive_path = "versions/{}".format(archive_name)
