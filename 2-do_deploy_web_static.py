7d05de449cf24de822131652f23e8b0ac364d4fe
olalekan <lekanmojibola@gmail.com>
2024-06-09 09:54:53 +0100

    God!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,7 +13,7 @@ from fabric.api import *
 import os
 
 
-env.hosts = ['ubuntu@34.232.72.189', 'ubuntu@54.160.114.58']
+env.hosts = ['34.232.72.189', '54.160.114.58']
 env.key_filename = '~/.ssh/id_rsa'
 env.user = 'ubuntu'
 
@@ -31,32 +31,34 @@ def do_deploy(archive_path):
     # Check if the archive exists
     if not os.path.exists(archive_path):
         return False
+    try:
+        # Extract the filename from the archive path
+        filename = os.path.basename(archive_path)
+        archive_name = os.path.splitext(filename)[0]
 
-    # Extract the filename from the archive path
-    filename = os.path.basename(archive_path)
-    archive_name = os.path.splitext(filename)[0]
+        # Upload the archive to /tmp/ directory of the web servers
+        put(archive_path, '/tmp/')
 
-    # Upload the archive to /tmp/ directory of the web servers
-    put(archive_path, '/tmp/')
+        # Uncompress the archive to /data/web_static/releases
+        # /<archive filename without extension>
+        run('mkdir -p /data/web_static/releases/{}'.format(archive_name))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.
+            format(filename, archive_name))
 
-    # Uncompress the archive to /data/web_static/releases
-    # /<archive filename without extension>
-    run('mkdir -p /data/web_static/releases/{}'.format(archive_name))
-    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.
-        format(filename, archive_name))
+        # Delete the archive from the web servers
+        run('rm /tmp/{}'.format(filename))
+        run('mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}'.
+            format(archive_name, archive_name))
+        run('rm -rf /data/web_static/releases/{}/web_static/'.format(archive_name))
+        # Delete the symbolic link /data/web_static/current
+        run('rm -rf /data/web_static/current')
 
-    # Delete the archive from the web servers
-    run('rm /tmp/{}'.format(filename))
-    run('mv /data/web_static/releases/{}/web_static/* \
-         /data/web_static/releases/{}'.
-        format(archive_name, archive_name))
-    run('rm -rf /data/web_static/releases/{}/web_static/'.format(archive_name))
-    # Delete the symbolic link /data/web_static/current
-    run('rm -rf /data/web_static/current')
+        # Create a new symbolic link /data/web_s
+        # tatic/current linked to the new version
+        run('ln -s /data/web_static/releases/{}/ \
+            /data/web_static/current'.format(archive_name))
 
-    # Create a new symbolic link /data/web_s
-    # tatic/current linked to the new version
-    run('ln -s /data/web_static/releases/{}/ \
-        /data/web_static/current'.format(archive_name))
-
-    return True
+        return True
+    except Exception as e:
+        return False
