7601e8611ca33b6067cf77c3d06c9f57b1d1c54f
olalekan <lekanmojibola@gmail.com>
2024-06-28 06:43:55 +0100

    dynamic rendering

--- test.py
+++ test.py
@@ -3,9 +3,9 @@ from models import storage
 from models.state import State
 
 # Retrieve all states
-states = storage.all(State)
-for state in states.values():
-    if state.name == 'Nigeria':
-        storage.delete(state)
-        storage.save()
+state_dict = storage.all()
+states = list(state_dict.values())
+states = sorted(states, key=lambda state: state.name)
+for state in states:
+    print(f"{state.id} and it's name {state.name}")
 # Print each state's id and name
